6ea663918e94484242eac198a4aa020334527ac6
dmitry <dmitrii_izmerov@epam.com>
2018-05-27 20:13:51 +0300

    added show book by id

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -3,13 +3,13 @@ package controllers;
 import models.Book;
 import play.data.Form;
 import play.data.FormFactory;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import repositories.BookRepository;
 import views.html.books.create;
 import views.html.books.edit;
 import views.html.books.index;
+import views.html.books.show;
 
 import javax.inject.Inject;
 import java.util.List;
@@ -28,18 +28,22 @@ public class BooksController extends Controller {
 
     public Result getAll() {
         List<Book> books = bookRepository.getAll();
-        return ok(Json.toJson(books));
+        return ok(index.render(books));
     }
 
     public Result getBookById(int bookId) {
-        return TODO;
+        Book book = bookRepository.findById(bookId);
+        if (Objects.isNull(book)) {
+            return notFound(String.format("Book with id=%d was not found.", bookId));
+        }
+        return ok(show.render(book));
     }
 
     public Result createBook() {
         Form<Book> bookForm = formFactory.form(Book.class).bindFromRequest();
         Book book = bookForm.get();
         bookRepository.save(book);
-        return redirect(routes.BooksController.indexForm());
+        return redirect(routes.BooksController.getAll());
     }
 
     public Result saveBook(int bookId) {
@@ -47,7 +51,7 @@ public class BooksController extends Controller {
         Book book = bookForm.get();
         book.setId(bookId);
         bookRepository.save(book);
-        return redirect(routes.BooksController.indexForm());
+        return redirect(routes.BooksController.getAll());
     }
 
     public Result deleteBook(int bookId) {
@@ -56,11 +60,6 @@ public class BooksController extends Controller {
 
     public Result getBookDetails(int bookId) {
         return TODO;
-    }
-
-    public Result indexForm() {
-        List<Book> books = bookRepository.getAll();
-        return ok(index.render(books));
     }
 
     public Result createBookForm() {
